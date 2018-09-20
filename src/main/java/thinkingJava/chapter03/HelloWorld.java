8d123c11e9c6604cd8a98d9b33b498631085fbd3
lizhongchen <themaner@qq.com>
2018-09-20 20:23:49 +0800

    Initial commit

---
+++ src/main/java/thinkingJava/chapter03/HelloWorld.java
@@ -0,0 +1,11 @@
+package thinkingJava.chapter03;
+/**
+ * @ TODO: 2018/9/12 目前的本地idea提示不能运行
+ * @ TODO: 2018/9/12 下一步研究把本地的git与idea的github连接起来
+ * @author 李重辰
+ */
+public class HelloWorld {
+  public static void main(String[] argv) {
+    System.out.println("Hello Word");
+  }
+}
---
+++ src/main/java/thinkingJava/chapter03/PassObject.java
@@ -0,0 +1,21 @@
+package thinkingJava.chapter03;
+
+class Letter{
+    char c;
+}
+public class PassObject {
+
+    private static void f(Letter y){
+        y.c = 'z';
+    }
+
+    public static void main (String[] args){
+        Letter x = new Letter();
+        x.c = 'a';
+        System.out.println("1:x.c: "+x.c);
+        Letter z = new Letter();
+        z.c = 'p';
+        f(z);
+        System.out.println("2:z.c: "+z.c);
+    }
+}
