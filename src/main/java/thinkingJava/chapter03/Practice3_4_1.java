d11d6d863ae601ffa974d6544a402939ac2ccdff
lizhongchen <themaner@qq.com>
2018-09-22 09:12:36 +0800

    See class name

---
+++ src/main/java/thinkingJava/chapter03/Practice3_4_1.java
@@ -0,0 +1,22 @@
+package thinkingJava.chapter03;
+
+public class Practice3_4_1 {
+    public static void main(String[] args){
+        for (int i=0; i<=100; i++) {
+            switch (i){
+                case 1:
+                    System.out.println(i);
+                case 2:
+                    System.out.println(i);
+                case 3:
+                    System.out.println(i);
+                case 4:
+                    System.out.println(i);
+                    break;
+            }
+            if (i==47)
+                return;
+
+        }
+    }
+}
