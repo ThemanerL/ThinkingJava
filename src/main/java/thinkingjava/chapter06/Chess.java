7bb38c0f1dd0217c15572fd9d7f4a4053af80768
Bepsun <themaner@qq.com>
2018-09-27 18:32:45 +0800

    Bepsun

---
+++ src/main/java/thinkingjava/chapter06/Chess.java
@@ -0,0 +1,8 @@
+package thinkingjava.chapter06;
+
+/**
+ * @author 李重辰
+ * @date 2018/9/27 17:58
+ */
+public class Chess {
+}
---
+++ src/main/java/thinkingjava/chapter06/PlaceSetting.java
@@ -0,0 +1,8 @@
+package thinkingjava.chapter06;
+
+/**
+ * @author 李重辰
+ * @date 2018/9/27 18:10
+ */
+public class PlaceSetting {
+}
