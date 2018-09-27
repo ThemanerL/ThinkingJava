04782ee8bb8f234bcae5c25cec0146ae467d4910
Bepsun <themaner@qq.com>
2018-09-27 17:04:31 +0800

    Bepsun

---
+++ src/main/java/thinkingjava/chapter05/PackagedClass.java
@@ -0,0 +1,11 @@
+package thinkingjava.chapter05;
+
+/**
+ * @author 李重辰
+ * @date 2018/9/27 11:10
+ */
+class PackagedClass {
+    PackagedClass(){
+        System.out.println("Creating a packaged class");
+    }
+}
---
+++ src/main/java/thinkingjava/chapter05/foreign/Foreign.java
@@ -0,0 +1,13 @@
+package thinkingjava.chapter05.foreign;
+
+/**
+ * 'thinkingjava.chapter05.PackagedClass' is not public in 'thinkingjava.chapter05'.
+ * Cannot be accessed from outside package
+ * @author 李重辰
+ * @date 2018/9/27 11:11
+ */
+public class Foreign {
+    public static void main(String[] args) {
+        //PackagedClass pc = new PackagedClass();
+    }
+}
