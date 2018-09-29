23a3e83d7e303854bc50ed95b0f9280499bc4ef0
Bepsun <themaner@qq.com>
2018-09-29 21:58:45 +0800

    Bepsun

---
+++ src/main/java/thinkingjava/chapter07/practice/Amphibian.java
@@ -0,0 +1,25 @@
+package thinkingjava.chapter07.practice;
+
+/**
+ * @author 李重辰
+ * @date 2018/9/29 19:49
+ */
+public class Amphibian {
+    protected void printName(Amphibian amphibian){
+        System.out.println("My name is ："+ amphibian.getClass().getSimpleName()+" from father");
+    }
+}
+
+class Frog extends Amphibian{
+
+    @Override
+    public void printName(Amphibian amphibian){
+        System.out.println("My name is ："+ amphibian.getClass().getSimpleName()+" from child");
+    }
+
+    public static void main(String[] args) {
+        Frog frog = new Frog();
+        Amphibian a = new Amphibian();
+        a.printName(frog);
+    }
+}
