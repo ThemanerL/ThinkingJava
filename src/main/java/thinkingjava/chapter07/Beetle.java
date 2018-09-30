40ca1764b41521d1e84b47791d0d48817e3abe46
Bepsun <themaner@qq.com>
2018-09-30 18:40:34 +0800

    Bepsun

---
+++ src/main/java/thinkingjava/chapter07/Beetle.java
@@ -0,0 +1,72 @@
+package thinkingjava.chapter07;
+
+/**
+ * 注释序号为代码执行顺序
+ * @author 李重辰
+ * @date 2018/9/30 16:38
+ */
+public class Beetle extends Insect {
+    /**
+     * 9
+     */
+    private int k = printInit("Beetle.k initialized");
+
+    /**
+     * 10
+     */
+    public Beetle() {
+        System.out.println("k = " + k);
+        System.out.println("j = " + j);
+    }
+
+    /**
+     * 3
+     */
+    private static int x2 = printInit("Static Beetle.x2 initialized");
+
+    public static void main(String[] args) {
+        // 4
+        System.out.println("Beetle constructor");
+
+        // 11           5
+        Beetle b = new Beetle();
+
+        // 第一个实体的创建或者对static成员的访问都有可能引起加载
+        Insect.printInit("asd");
+
+    }
+}
+
+class Insect {
+
+    /**
+     * 6
+     */
+    private int i = 9;
+
+    /**
+     * 7
+     */
+    int j;
+
+    /**
+     * 8
+     */
+    Insect() {
+        System.out.println("i = " + i + " , j = " + j);
+        j = 39;
+    }
+
+    /**
+     * 1
+     */
+    private static int x1 = printInit("Static Insect.x1 initialized");
+
+    /**
+     * 2 加载
+     */
+    static int printInit(String s) {
+        System.out.println(s);
+        return 47;
+    }
+}
