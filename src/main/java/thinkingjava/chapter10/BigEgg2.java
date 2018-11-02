25094502c84f5e6e9b2cb6dcded289e006376272
ThemanerL <themaner@qq.com>
2018-11-02 17:35:23 +0800

    InnerClass finash!

---
+++ src/main/java/thinkingjava/chapter10/BigEgg2.java
@@ -0,0 +1,50 @@
+package thinkingjava.chapter10;
+
+/**
+ * 外围类继承外围类，内部类继承内部类
+ * @author 李重辰
+ * @date 2018/11/2 14:55
+ */
+public class BigEgg2 extends Egg2 {
+    public class Yolk extends Egg2.Yolk{
+        public Yolk(){
+            System.out.println("BigEgg2.Yolk");
+        }
+        @Override
+        public void f(){
+            System.out.println("BigEgg2.Yolk.f");
+        }
+    }
+
+    public BigEgg2(){
+            insertYolk(new Yolk());
+    }
+
+    public static void main(String[] args) {
+        Egg2 egg2 = new BigEgg2();
+        egg2.g();
+    }
+
+}
+
+class Egg2{
+    public class Yolk{
+        public Yolk (){
+            System.out.println("BigEgg2.Yolk");
+        }
+        public void f(){
+            System.out.println("BigEgg2.Yolk.f");
+        }
+    }
+
+    private Yolk y = new Yolk();
+    public Egg2 (){
+        System.out.println("new Egg2()");
+    }
+    public void insertYolk(Yolk y){
+        this.y = y;
+    }
+    public void g(){
+        y.f();
+    }
+}
