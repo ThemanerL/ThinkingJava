28f3761c3274aba87ed58fa637d594ae474e0fa0
李重辰 <32975859+themanerl@users.noreply.github.com>
2018-11-13 21:58:21 +0800

    Override equals/hashCode

--- src/main/java/thinkingjava/thirdparty/typeinfo/pets/Person.java
+++ src/main/java/thinkingjava/thirdparty/typeinfo/pets/Person.java
@@ -8,4 +8,14 @@ public class Person extends Individual {
   public Person(String name) {
     super(name);
   }
+
+  @Override
+  public boolean equals(Object o) {
+    return super.equals(o);
+  }
+
+  @Override
+  public int hashCode() {
+    return super.hashCode();
+  }
 }
