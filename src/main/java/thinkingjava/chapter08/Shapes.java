a775043ae8d5cf1cb97d4c4c8a8a61507fb1ab07
acer <themaner@qq.com>
2018-11-21 17:54:47 +0800

    增加适配Iterable,支持生成多个对象,支持使用foreach遍历对象

--- src/main/java/thinkingjava/chapter08/Shapes.java
+++ src/main/java/thinkingjava/chapter08/Shapes.java
@@ -1,13 +1,16 @@
 package thinkingjava.chapter08;
 
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Random;
+import java.util.Set;
 
 /**
  * @author 李重辰
  * @date 2018/10/1 21:49
  */
 public class Shapes {
-  private static RandomShapeGenerator generator = new RandomShapeGenerator();
+  private static RandomShapeGenerator generator = RandomShapeGenerator.getInstance;
 
   public static void main(String[] args) {
     Shape[] s = new Shape[9];
@@ -19,11 +22,43 @@ public class Shapes {
       shp.draw();
       shp.printName();
     }
+
+    System.out.println("________________");
+    new Shapes().chapterElevenP31();
+  }
+
+  private void chapterElevenP31(){
+    RandomShapeGenerator generator1 = new RandomShapeGenerator(8);
+    for (Object s:generator1) {
+      System.out.print(s.getClass().getSimpleName() + " ");
+    }
   }
 }
 
-class RandomShapeGenerator {
+/**
+ * 2018/11/21 修改,增加适配Iterable,支持生成多个对象
+ */
+class RandomShapeGenerator implements Iterable {
   private Random rand = new Random(System.currentTimeMillis());
+  private int count = 0;
+  private Set<Shape> shapes = null;
+  static RandomShapeGenerator getInstance = new RandomShapeGenerator();
+
+  private RandomShapeGenerator() {
+  }
+
+  RandomShapeGenerator(int count) {
+    this.count = count;
+    shapes = getShapes();
+  }
+
+  private Set<Shape> getShapes(){
+    Set<Shape> set = new HashSet<>();
+    for (int i = 0; i < count; i++) {
+      set.add(next());
+    }
+    return set;
+  }
 
   Shape next() {
     switch (rand.nextInt(3)) {
@@ -36,5 +71,21 @@ class RandomShapeGenerator {
       default:
         return null;
     }
+  }
+
+  @Override
+  public Iterator<Shape> iterator() {
+    return new Iterator<Shape>() {
+      int index = 0;
+      @Override
+      public boolean hasNext() {
+        return index < count;
+      }
+
+      @Override
+      public Shape next() {
+        return (Shape)shapes.toArray()[index++];
+      }
+    };
   }
 }
