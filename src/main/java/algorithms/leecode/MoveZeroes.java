79664fe0e15d058f5b69157881bf4beee22cd786
李重辰 <themaner@qq.com>
2019-08-21 20:51:40 +0800

    新增题目，移动零

---
+++ src/main/java/algorithms/leecode/MoveZeroes.java
@@ -0,0 +1,27 @@
+package algorithms.leecode;
+
+/**
+ * 移动零
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: [0,1,0,3,12]
+ * 输出: [1,3,12,0,0]
+ * 说明:
+ * <p>
+ * 必须在原数组上操作，不能拷贝额外的数组。
+ * 尽量减少操作次数。
+ *
+ * @author 李重辰
+ * @date 2019/8/21 20:50
+ */
+public class MoveZeroes {
+  public static void main(String[] args) {
+
+  }
+
+  private void solution(int[] nums){
+
+  }
+}
