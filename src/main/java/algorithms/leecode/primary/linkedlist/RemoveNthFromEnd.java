f03e846ef22d14138b706a0ac01b6e02132f1d2a
李重辰 <themaner@qq.com>
2019-12-05 17:50:08 +0800

    Update RemoveNthFromEnd.java

--- src/main/java/algorithms/leecode/primary/linkedlist/RemoveNthFromEnd.java
+++ src/main/java/algorithms/leecode/primary/linkedlist/RemoveNthFromEnd.java
@@ -1,7 +1,5 @@
 package algorithms.leecode.primary.linkedlist;
 
-import static algorithms.leecode.primary.linkedlist.ListNode;
-
 /**
  * <h1>删除链表的倒数第N个节点</h1>
  * 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
