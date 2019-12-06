03369e838ec601e9b429636bb36492356b5fcaa8
李重辰 <themaner@qq.com>
2019-12-06 21:09:28 +0800

    新增题目，回文链表

---
+++ src/main/java/algorithms/leecode/primary/linkedlist/IsPalindrome.java
@@ -0,0 +1,25 @@
+package algorithms.leecode.primary.linkedlist;
+
+/**
+ * 请判断一个链表是否为回文链表。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: 1->2
+ * 输出: false
+ * 示例 2:
+ * <p>
+ * 输入: 1->2->2->1
+ * 输出: true
+ * 进阶：
+ * 你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？
+ * 提示：此题可以使用递归解决。
+ *
+ * @author 李重辰
+ * @date 2019/12/6 21:08
+ */
+public class IsPalindrome {
+  public boolean isPalindrome(ListNode head) {
+    return false;
+  }
+}
