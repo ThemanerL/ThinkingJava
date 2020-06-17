b337873a499c287288aee812b23cb1256ff39e54
李重辰 <themaner@qq.com>
2020-06-17 14:09:28 +0800

    修复创建线程错误

--- src/main/java/multithread/UserThreadFactory.java
+++ src/main/java/multithread/UserThreadFactory.java
@@ -22,12 +22,21 @@ public class UserThreadFactory implements ThreadFactory {
     this.namePrefix = "From User ThreadFactory's " + whatFeatureOfGroup + "-Worker-";
   }
 
+//  @Override
+//  public Thread newThread(@NotNull Runnable r) {
+//    return new Thread(() -> {
+//      String name = namePrefix + nextId.getAndIncrement();
+//      Thread thread = new Thread(null, r, name, 0, false);
+//      thread.start();
+//      System.out.println("Thread Name：" + thread.getName() + "\n HashCode：" + r.hashCode());
+//    });
+//  }
+
   @Override
   public Thread newThread(@NotNull Runnable r) {
-    return new Thread(() -> {
-      String name = namePrefix + nextId.getAndIncrement();
-      Thread thread = new Thread(null, r, name, 0, false);
-      System.out.println(thread.getName());
-    });
+    String name = namePrefix + nextId.getAndIncrement();
+    Thread thread = new Thread(null, r, name, 0, false);
+    System.out.println("Thread Name：" + thread.getName() + "\n HashCode：" + r.hashCode());
+    return thread;
   }
 }
